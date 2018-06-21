0a8d39e17465762496bbbc102b39bf2a0c69667f
Jan Hořava <czechinthemail@gmail.com>
2018-06-20 19:32:24 -0700

    added contextmenu preventDefault

--- js/main.js
+++ js/main.js
@@ -46,6 +46,9 @@ function repo_init(){
       },
       'menu': true,
       'mousebinds': {
+        'contextmenu': {
+          'preventDefault': true,
+        },
         'mousemove': {
           'preventDefault': true,
           'todo': webgl_camera_handle,
