119b2c56229d2daf47fa6d328e62bb01024a3ec4
Jan Hořava <czechinthemail@gmail.com>
2023-07-18 13:54:24 -0700

    combined js into repo.js

--- repo.js
+++ repo.js
@@ -1,5 +1,18 @@
 'use strict';
 
+function charactersheet_update(){
+    core_ui_update({
+      'class': true,
+      'ids': {
+        'experience': webgl_characters[webgl_character_id]['experience'],
+        'health-max': webgl_characters[webgl_character_id]['health-max'],
+        'jump-height': webgl_characters[webgl_character_id]['jump-height'],
+        'level': webgl_characters[webgl_character_id]['level'],
+        'speed': webgl_characters[webgl_character_id]['speed'],
+      },
+    });
+}
+
 function repo_escape(){
     if(webgl_character_level() < 0){
         return;
