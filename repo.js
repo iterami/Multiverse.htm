0f23709da50508c6095cdb7b1b7b74b6bba5f270
Jan Hořava <czechinthemail@gmail.com>
2025-03-15 07:54:21 -0700

    updated mousemove and mousewheel event usage

--- repo.js
+++ repo.js
@@ -113,19 +113,14 @@ function repo_init(){
         },
         'mousemove': {
           'todo': function(){
-              webgl_controls_mouse(webgl_character_id);
+              webgl_controls_mouse();
           },
         },
         'mouseup': {
           'todo': webgl_pick_entity,
         },
         'wheel': {
-          'todo': function(event){
-              webgl_controls_wheel(
-                webgl_character_id,
-                event.deltaY
-              );
-          },
+          'todo': webgl_controls_wheel,
         },
       },
       'tabs': {
