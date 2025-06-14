f08b39b80172e39f1d0c84de7d7de9e0e4348736
Jan Hořava <czechinthemail@gmail.com>
2025-06-14 02:59:18 -0700

    simplified property usage

--- repo.js
+++ repo.js
@@ -63,7 +63,7 @@ function repo_init(){
                         'character': 0,
                         'json': JSON.parse(event.target.result),
                       })){
-                        document.title = (webgl_properties['title'] || element.files[0].name) +  ' - ' + core_repo_title;
+                        document.title = (webgl_properties.title || element.files[0].name) +  ' - ' + core_repo_title;
 
                     }else{
                         element.value = null;
@@ -86,8 +86,8 @@ function repo_init(){
                 'character': 0,
                 'json': level_json,
               });
-              document.title = level_json['title']
-                ? level_json['title'] + ' - ' + core_repo_title
+              document.title = level_json.title
+                ? level_json.title + ' - ' + core_repo_title
                 : core_repo_title;
           },
         },
@@ -105,7 +105,7 @@ function repo_init(){
       'keybinds': {
         'Backquote': {
           'todo': function(){
-              webgl_characters[webgl_character_id]['automove'] = !webgl_characters[webgl_character_id]['automove'];
+              webgl_characters[webgl_character_id].automove = !webgl_characters[webgl_character_id].automove;
           },
         },
       },
@@ -176,7 +176,7 @@ function update_ui(){
         'level-goal': core_number_format({
             'decimals-max': 0,
             'decimals-min': 0,
-            'number': Math.floor(webgl_characters[webgl_character_id]['level'] + 1) * 1e3,
+            'number': Math.floor(webgl_characters[webgl_character_id].level + 1) * 1e3,
           }),
       },
     });
