3efbb8b44efef4ebf894a3e311ec0559fba8ac58
Jan Hořava <czechinthemail@gmail.com>
2025-06-19 13:49:09 -0700

    changed storage-controls to storage_controls

--- repo.js
+++ repo.js
@@ -126,7 +126,7 @@ function repo_init(){
           'todo': webgl_controls_wheel,
         },
       },
-      'storage-controls': true,
+      'storage_controls': true,
       'tabs': {
         'load': {
           'content': '<button id=character-random type=button>Create Random Character</button><br>'
