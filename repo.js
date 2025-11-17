ed6dfbf6dab6c792db7d613cf1e852f3862883c3
Jan Hořava <czechinthemail@gmail.com>
2025-11-17 07:30:20 -0800

    opened core menu in beforeunload

--- repo.js
+++ repo.js
@@ -5,6 +5,7 @@ function repo_init(){
       'beforeunload': {
         'todo': function(event){
             if(webgl !== 0){
+                core_escape(true);
                 event.preventDefault();
             }
         },
