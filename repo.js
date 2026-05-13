4f0bb82e946235ea2e86218c8e5c1d0f6d7135bd
Jan Hořava <czechinthemail@gmail.com>
2026-05-13 14:02:16 -0700

    updated to webgl_pick()

--- repo.js
+++ repo.js
@@ -123,7 +123,7 @@ function repo_init(){
           },
         },
         'pointerup': {
-          'todo': webgl_pick_entity,
+          'todo': webgl_pick,
         },
         'wheel': {
           'todo': webgl_controls_wheel,
